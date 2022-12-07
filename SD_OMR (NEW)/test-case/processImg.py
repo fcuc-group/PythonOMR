4ac559ce25744eb3640db40663ff723c2809a68b
David Gong <davidgonghongshen@gmail.com>
2022-12-07 17:51:48 +0800

    1

--- SD_OMR (NEW)/test-case/processImg.py
+++ SD_OMR (NEW)/test-case/processImg.py
@@ -8,19 +8,22 @@ from PIL import Image
 # - MixBlur_Half_2.png
 # - MixSymbol.png
 # - MixSymbol2.png
-# - All.png
+# - all.png
 # - all_2.png
 
-testImg = "All.png"
+testImg = "all_2.png"
 
 im = Image.open(testImg)
 x, y = im.size
 
 qrcode = Image.open("qrcode.png")
-qrcode = qrcode.resize((250, 250))
+qrcode = qrcode.resize((100, 100))
 image = Image.new("RGB", (x, y), (255, 255, 255))
 image.paste(im, (0, 0))
-image.paste(qrcode, (1200, 70))
+print(im.size)
+print(x)
+print(y)
+image.paste(qrcode, (550,40))
 
-image.show()
-# image.save("omr/"+testImg)
+#image.show()
+image.save("omr/"+testImg)
